1c22dcf5568de67f38c69b45ab1954fdaacfe0e6
John Valentino <jvalentino2@gmail.com>
2023-03-16 14:48:30 -0500

    initial port

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm delete --wait pg-secondary || true
+helm install pg-secondary \
+	--wait \
+	--set auth.postgresPassword=postgres \
+	--set auth.username=postgres \
+	--set auth.database=dw \
+	bitnami/postgresql
+sh -x ./verify.sh
